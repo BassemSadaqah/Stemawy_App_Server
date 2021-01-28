f587a7ab71b43bec068486945d29d1321924320b
Bassem Sadaqah <bassem.sadaqah@gmail.com>
2021-01-28 18:55:28 +0200

    Privacy Policy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const expressGraphQL=require('express-graphql').graphqlHTTP
 const schema=require('./schema/schema');
 const e = require("express");
 const PORT = process.env.PORT || 5000;
-
+app.use(express.static('public'));
 app.use(bodyParser.json({
     limit: '50mb',
     extended: true
