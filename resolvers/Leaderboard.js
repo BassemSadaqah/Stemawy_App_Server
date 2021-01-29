e2e4fac09e502f7fe0075463433795bddcd063b2
Bassem Sadaqah <bassem.sadaqah@gmail.com>
2021-01-30 01:41:37 +0200

    Add user Id to Leaderboard resolver

--- resolvers/Leaderboard.js
+++ resolvers/Leaderboard.js
@@ -5,7 +5,7 @@ const JsonError = require('./JsonError')
 module.exports=(parent,args,req)=>{
     // if(!req.isAuth) throw new JsonError({code:400,msg:'Not Authorized'})
     var count=count?sqli(args.count):10
-    return client.query(`SELECT first_name,last_name,email,fb_id,points FROM users where points is not NULL ORDER BY points DESC limit 10`)
+    return client.query(`SELECT id,first_name,last_name,email,fb_id,points FROM users where points is not NULL ORDER BY points DESC limit 10`)
     .then(RES=>{
         if(RES.rows.length==0) return []
         let users=RES.rows
